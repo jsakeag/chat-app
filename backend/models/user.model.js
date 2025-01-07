8f593032d53bdd6163e5fdb06ee62c25fd404003
Keagan Aromin <keaganaromin@Keagans-MacBook-Pro.local>
2025-01-07 11:27:41 -0800

    Added user.model.js

---
+++ backend/models/user.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+	{
+		fullName: {
+			type: String,
+			required: true,
+		},
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+			minlength: 6,
+		},
+		gender: {
+			type: String,
+			required: true,
+			enum: ["male", "female"],
+		},
+		profilePic: {
+			type: String,
+			default: "",
+		},
+	},
+	{ timestamps: true } // automatically adds createdAt and updatedAt to object, which are set to current time on document start and update
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
