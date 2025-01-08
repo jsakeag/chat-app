884a9989df812834007363c86ff7777dc4eee8a0
Michelle Li <mmli@berkeley.edu>
2025-01-08 01:01:30 -0800

    setup auth routes, setup mongodb, created signup + login + logout endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 
-const app = express();
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
-dotenv.config();
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-    // root route http://localhost:5000/
-    res.send("Hello World!!");
-});
+dotenv.config();
 
-app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+
+app.use("/api/auth", authRoutes);
+
+// app.get("/", (req, res) => {
+//     // root route http://localhost:5000/
+//     res.send("Hello World!!");
+// });
+
+app.listen(PORT, () => {
+    connectToMongoDB();
+    console.log(`Server Running on port ${PORT}`)
+});
