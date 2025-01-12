030e005658da5fdd4ecdf418b2f40eec499a32e5
Michelle Li <mmli@berkeley.edu>
2025-01-12 01:24:16 -0800

    Fix search bar and time bugs

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -1,28 +1,30 @@
 import { useState } from "react";
 import { IoSearchSharp } from "react-icons/io5";
-import useConversation from '../../zustand/useConversation'
-import useGetConversations from '../../hooks/useGetConversations'
+import useConversation from "../../zustand/useConversation";
+import useGetConversations from "../../hooks/useGetConversations";
 import toast from "react-hot-toast";
 
 const SearchInput = () => {
   const [search, setSearch] = useState("");
   const { setSelectedConversation } = useConversation();
-  const { conversations} = useGetConversations();
+  const { conversations } = useGetConversations();
 
   const handleSubmit = (e) => {
-    console.log("submit")
+    console.log("submit");
     e.preventDefault();
     if (!search) return;
     if (search.length < 3) {
-      return toast.error("Search term must be at least 3 characters long")
+      return toast.error("Search term must be at least 3 characters long");
     }
 
-    const conversation = conversations.find((c) => c.fullName.toLowerCase().includes(search.toLowerCase()));
-    
+    const conversation = conversations.find((c) =>
+      c.fullName.toLowerCase().includes(search.toLowerCase())
+    );
+
     if (conversation) {
-      setSelectedConversation(conversation)
-      setSearch('asd')
-    } else toast.error("No such user found")
+      setSelectedConversation(conversation);
+      setSearch("");
+    } else toast.error("No such user found");
   };
   return (
     <form onSubmit={handleSubmit} className="flex items-center gap-2">
